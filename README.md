213064dc8c618e80e1e2a35363838145ff4516d9
Lance Galletti <galletti@bu.edu>
2021-03-24 13:48:35 -0400

    adding linear decision boundary example

--- README.md
+++ README.md
@@ -24,6 +24,8 @@ Produces the following graph:
 
 ### Node Activation Visualization
 
+#### XOR Function
+
 ```python
     model = keras.models.Sequential()
     model.add(layers.Dense(3, input_dim=2, activation='sigmoid'))
@@ -43,7 +45,7 @@ Produces the following graph:
     dg.render(input=X)
 ```
 
-Produces the following decision boundary:
+Produces the following decision boundary (visualized using `matplotlib`):
 
 <p align="center">
     <img src="examples/xor_model.png" width=50% height='auto'>
@@ -54,3 +56,42 @@ And the following GIF:
 <p align="center">
     <img src="examples/xor_model.gif"/>
 </p>
+
+The darker the node the higher the activation is at that node.
+
+#### Linear Decisions Boundary
+
+```python
+    import sklearn.datasets as datasets
+
+    model = keras.models.Sequential()
+    model.add(layers.Dense(3, input_dim=2, activation=ACTIVATION))
+    model.add(layers.Dense(1, activation=ACTIVATION))
+    model.compile(loss="binary_crossentropy")
+
+    centers = [[.5, .5]]
+    t, _ = datasets.make_blobs(n_samples=50, centers=centers, cluster_std=.1)
+    X = np.array(t)
+    Y = np.array([1 if x[0] - x[1] >= 0 else 0 for x in X])
+
+    history = model.fit(X, Y)
+
+    dg = DenseGraph(model)
+    dg.render(input=X, duration=.3)
+```
+
+Produces the following decision boundary (visualized using `matplotlib`):
+
+<p align="center">
+    <img src="examples/line_model.png" width=50% height='auto'>
+</p>
+
+And the following GIF:
+
+<p align="center">
+    <img src="examples/line_model.gif"/>
+</p>
+
+At a glance you can see that the activations of the middle hidden node
+results in predictions of class 0 while the activation of the left-most
+and right-most hiddent nodes result in predictions of class 1.
--- tests/test_viz.py
+++ tests/test_viz.py
@@ -38,8 +38,8 @@ def test_dense_input_xor():
     ax.scatter(X[:,0],X[:,1], color=colors[Y].tolist(), s=50, alpha=0.8)
 
     h = .02
-    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + 1
-    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + 1
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                          np.arange(y_min, y_max, h))
     meshData = np.c_[xx.ravel(), yy.ravel()]
@@ -92,7 +92,6 @@ def test_dense_input_line():
         [ 0.58046833, 0.36303798],
         [ 0.57127328, 0.64131463],
         [ 0.95395092, 0.20912687],
-        [-0.01059796, 0.63072372],
         [ 0.52538242, 0.58039787],
         [ 0.43768949, 0.51123307],
         [ 0.26697003, 0.6801653 ],
@@ -116,19 +115,21 @@ def test_dense_input_line():
         [ 0.53548523, 0.41964381]])
     Y = np.array([1 if x[0] - x[1] >= 0 else 0 for x in X])
 
-    history = model.fit(X, Y, batch_size=50, epochs=100)
+    history = model.fit(X, Y, batch_size=49, epochs=100)
 
     # see which nodes activate for a given class
     X0 = X[X[:,0] - X[:,1] <= 0]
     X1 = X[X[:,0] - X[:,1] >= 0]
 
+    X = np.concatenate((X0, X1), axis=0)
+
     fig, ax = plt.subplots()
     ax.scatter(X0[:,0],X0[:,1], color='b', s=10, alpha=0.8)
     ax.scatter(X1[:,0],X1[:,1], facecolors='none', edgecolors='black')
 
-    h = .02
-    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + 1
-    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + 1
+    h = .01
+    x_min, x_max = 0, 1
+    y_min, y_max = 0, 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                          np.arange(y_min, y_max, h))
     meshData = np.c_[xx.ravel(), yy.ravel()]
@@ -142,4 +143,4 @@ def test_dense_input_line():
     fig.savefig("test_model_line.png")
 
     dg = DenseGraph(model)
-    dg.render(input=X0, filename='test_input_line', duration=.1)
+    dg.render(input=X, filename='test_input_line', duration=.3)
