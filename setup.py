d9d63d2cb17bf58c73d8ffdaa70d2566d6cea597
Lance Galletti <galletti@bu.edu>
2021-10-14 17:03:18 -0400

    kviz 0.0.5 release
    Signed-off-by: Lance Galletti <galletti@bu.edu>

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ tests_require = [
 setup(
   name='kviz',
   packages=['kviz'],
-  version='0.0.4',
+  version='0.0.5',
   description='A Library for visualizing keras neural networks',
   install_requires=[
     'tf-nightly',
